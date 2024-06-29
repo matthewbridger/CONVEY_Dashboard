22c08db2a29ba3e433befcd84acd198b5af8fe66
Matthew Bridger <matthewbridger2000@gmail.com>
2024-06-29 09:17:49 +0100

    Updated router with the new before Each to sign the user out

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PrivateRoutes from './PrivateRoutes';
+import PublicRoutes from './PublicRoutes';
+import { useUserStore } from '../store/user.js';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,12 +10,29 @@ const router = createRouter({
       path: '/:pathMatch(.*)*',
       component: () => import('../views/error/404.vue')
     },
+    PublicRoutes,
     PrivateRoutes
   ]
 })
 
 router.beforeEach(async (to, from, next) => {
-  next();
-});
+  const { is_current_session_valid } = useUserStore();
 
+  const is_valid_user_session = await is_current_session_valid();
+  
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+      if(is_valid_user_session == true) {
+          next();
+      }
+      else {
+          return next({ name: 'SignIn' });
+      }
+  }
+  else {
+      if(is_valid_user_session == true && (to.name == 'SignIn' || to.name == 'SignUp')) {
+          return next({ name: 'Dashboard' });
+      }
+      next();
+  }
+});
 export default router
